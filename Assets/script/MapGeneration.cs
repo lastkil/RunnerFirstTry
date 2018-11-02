bb7f4d655aedf33ad6ef36c645d1390abb65a2ca
nastywolf <jeanlou.ettinger@gmail.com>
2018-11-02 23:48:23 +0100

    Map du proto
    Modélisation de la map

---
+++ Assets/script/MapGeneration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneration : MonoBehaviour {
+
+    public GameObject _piece;
+    public Transform _transform;
+    public int _limitSpawn;
+
+    private int _nbSpawn;
+
+    // Update is called once per frame
+    public void Update () {
+		if(_nbSpawn < _limitSpawn)
+        {
+            SpawnPiece();
+        }
+	}
+    
+    private void SpawnPiece()
+    {
+        GameObject piece = (GameObject)Instantiate(_piece, _transform);
+        _nbSpawn++;
+    }
+}
